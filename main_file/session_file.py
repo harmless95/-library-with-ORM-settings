8fef0bab712ca6bb398a930accae1241ebf38957
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-06-06 16:07:33 +0700

    specified the echo true flag to view queries in the db

--- main_file/session_file.py
+++ main_file/session_file.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///library_base.db")
+engine = create_engine("sqlite:///library_base.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
